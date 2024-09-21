5fab7ef822918b2e384e85504dbe4562767ea93e
chrisreeves70 <chrisreeves70@icloud.com>
2024-09-21 16:44:53 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
 // Simple index file to serve as the entry point
-echo "Welcome to the Multi-User Blog System!";
+echo "<h1>Welcome to the Multi-User Blog System!</h1>";
+echo "<ul>";
+echo "<li><a href='register.php'>Register</a></li>";
+echo "<li><a href='login.php'>Login</a></li>";
+echo "<li><a href='create_post.php'>Create Post</a></li>";
+echo "<li><a href='view_posts.php'>View Posts</a></li>";
+echo "</ul>";
 ?>
